66e214dd9c312a30528c109136fed3fae3b020c1
willmattis <112517610+willmattis@users.noreply.github.com>
2022-11-28 18:49:51 -0500

    Add files via upload

---
+++ Lab11/build/CMakeFiles/applibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/applibrary.dir/whattotest.cpp.o"
+  "libapplibrary.pdb"
+  "libapplibrary.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/applibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab11/whattotest.cpp
@@ -0,0 +1,61 @@
+// whattotest.cpp
+
+#include "whattotest.h"
+ 
+WidgetWithArrays::WidgetWithArrays() : m_count(0)
+{
+    for (int i =0 ; i < 100; i++) 
+    {
+        m_array[i] = 0;
+    }
+}
+
+
+void WidgetWithArrays::storeInteger(int a)
+{
+    //TODO:
+    m_array[m_count++] = a;
+}
+  
+int WidgetWithArrays::sum()
+{
+    int retVal = 0;
+    // TODO:
+    // hint standard for loop OR
+    // try looking into range based for loop (hint it can work with dumb arrays)
+    for (int n : m_array)
+    {
+        retVal += n;
+    }
+
+    return retVal;
+}
+ 
+
+WidgetWithVector::WidgetWithVector()
+{
+
+
+}
+
+void WidgetWithVector::storeInteger(int a)
+{
+    m_vector.push_back(a);
+
+}
+
+int WidgetWithVector::sum()
+{
+    int retVal = 0;
+
+    for(int n : m_vector)
+    {
+        retVal = retVal +n;
+    }
+
+
+    return retVal;
+}
+ 
+
+
